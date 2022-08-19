fe6403bbf43c9db55cf7e9ebab286cdb7a7c99fc
JoahanC <joahan.castanedajaimes@gmail.com>
2022-08-19 11:43:25 -0700

    Removed unecessary files

--- best_fits/findings_pdf.py
+++
@@ -1,8 +0,0 @@
-"""
-This file generates a pdf with all the plots and findings from the MCMC
-modeling runs.
-"""
-import os
-from table_generator import generate_MCMC_results
-
-generate_MCMC_results()
